9fc8243d5219c1199e50ba7c1b8b880c65dd9958
chordpli <chordplaylist@gmail.com>
2023-01-12 02:30:35 +0900

    build: build project

---
+++ src/main/java/com/exredis/ExRedisApplication.java
@@ -0,0 +1,13 @@
+package com.exredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExRedisApplication.class, args);
+    }
+
+}
